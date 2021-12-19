299fe6d7629abe0ce0d4d39687e86f28b2b5ae55
LodolaTomas <tomaslodola1@gmail.com>
2021-12-19 00:08:10 -0300

    subiendoHomeAboutSkills

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, Renderer2, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LodoaTomasPorfilio';
+  objectKeys = Object.keys;
+  navlist: any={
+    home:'Home',
+    about:'AboutMe',
+    skills:'Skills',
+    projects:'Projects',
+    contact:'ContactMe',
+  };
+
+  @ViewChildren('asList') list?: QueryList<ElementRef> ;
+
+  constructor(private render2: Renderer2) {
+    
+  }
+
+  ngAfterViewInit() {
+    this.list?.toArray()[0].nativeElement.classList.add('active');
+  }
+  
+  change(i:any){
+    const elementList= this.list?.toArray()[i].nativeElement;
+    this.list?.forEach(element => {
+      this.render2.removeClass(element.nativeElement, 'active');
+    });
+    this.render2.addClass(elementList,'active');
+  }
 }
